0ca0716188ed360ad3f7b1f657f40d4db41d25f4
RajKKapadia <raajforyou@gmail.com>
2023-04-19 10:44:27 +0530

    started working on image editor

---
+++ image_editor/settings.py
@@ -0,0 +1,18 @@
+ROTATE_DEFAULT = 0
+ZOOM_DEFAULT = 0
+FLIP_OPTIONS = ['None', 'X', 'Y', 'Both']
+BLUR_DEFAULT = 0
+CONTRAST_DEFAULT = 0
+BRIGHTNESS_DEFAULT = 1
+VIBRANCE_DEFAULT = 1
+GRAYSCALE_DEFAULT = 1
+INVERT_DEFAULT = False
+EFFECT_OPTIONS = ['None', 'Emboss', 'Find edges', 'Contour', 'Edge enhance']
+
+WHITE = '#FFF'
+GREYY = 'grey'
+BLUE = '#1f6aa5'
+DARK_GREY = '#4a4a4a'
+CLOSE_RED = '#8a0606'
+
+SLIDER_BG = '#64686b'
