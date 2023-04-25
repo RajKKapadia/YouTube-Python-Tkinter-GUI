fad2a767bb103d8e844eeee984407ea7cee438e3
RajKKapadia <raajforyou@gmail.com>
2023-04-25 10:58:30 +0530

    skeleton ready

--- image_editor/menu.py
+++ image_editor/menu.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
 
-from panels import SliderPanel
+from panels import SliderPanel, SegmentedPanel, SwitchPanel, DropDownPanel
+from settings import *
 
 class Menu(ctk.CTkTabview):
-    def __init__(self, parent, rotation, zoom):
+    def __init__(self, parent, pos_vars, color_vars, effect_vars):
         super().__init__(master=parent)
         self.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
 
@@ -14,29 +15,38 @@ class Menu(ctk.CTkTabview):
         self.add('Export')
 
         # Widgets
-        PositionFame(self.tab('Position'), rotation, zoom)
-        ColorFame(self.tab('Colors'))
-        EffectsFame(self.tab('Effects'))
+        PositionFame(self.tab('Position'), pos_vars)
+        ColorFame(self.tab('Colors'), color_vars)
+        EffectsFame(self.tab('Effects'), effect_vars)
         ExportFame(self.tab('Export'))
 
         
 class PositionFame(ctk.CTkFrame):
-    def __init__(self, parent, rotation, zoom):
+    def __init__(self, parent, pos_vars):
         super().__init__(master=parent, fg_color='transparent')
         self.pack(expand=True, fill='both')
 
-        SliderPanel(self, 'Rotation', rotation, 0, 360)
-        SliderPanel(self, 'Zoom', zoom, 0, 100)
+        SliderPanel(self, 'Rotation', pos_vars['rotate'], 0, 360)
+        SliderPanel(self, 'Zoom', pos_vars['zoom'], 0, 100)
+        SegmentedPanel(self, 'Invert', pos_vars['flip'], FLIP_OPTIONS)
 
 class ColorFame(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, color_vars):
         super().__init__(master=parent, fg_color='transparent')
         self.pack(expand=True, fill='both')
 
+        SwitchPanel(self, (color_vars['grayscale'], 'B/W'), (color_vars['invert'], 'Invert'))
+        SliderPanel(self, 'Brightness', color_vars['brightness'], 0 ,5)
+        SliderPanel(self, 'Vibrance', color_vars['vibrance'], 0 ,5)
+
 class EffectsFame(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, effect_vars):
         super().__init__(master=parent, fg_color='transparent')
         self.pack(expand=True, fill='both')
+
+        DropDownPanel(self, effect_vars['effects'], EFFECT_OPTIONS)
+        SliderPanel(self, 'Blue', effect_vars['blur'], 0 ,3)
+        SliderPanel(self, 'Contrast', effect_vars['contrast'], 0 ,10)
 
 class ExportFame(ctk.CTkFrame):
     def __init__(self, parent):
